64dd3b4e343c3101e4306153324b149d9e4e8497
xiaohu <1446216110@qq.com>
2022-05-20 10:13:52 +0800

    使用Spring Cache 缓存套餐数据。

--- src/main/java/com/xiaohu/reggie/controller/SetmealController.java
+++ src/main/java/com/xiaohu/reggie/controller/SetmealController.java
@@ -13,6 +13,9 @@ import com.xiaohu.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +35,7 @@ public class SetmealController {
 
 
     @PostMapping
+    @CacheEvict(value="setmealCache",allEntries = true)
     public R<String> saveSetmeal(@RequestBody SetmealDto setmealDto) {
         log.info("套餐:{}", setmealDto);
         setmealService.saveSetmealWithDish(setmealDto);
@@ -75,6 +79,7 @@ public class SetmealController {
      * @param ids
      * @return
      */
+    @CacheEvict(value="setmealCache",allEntries = true)
     @DeleteMapping
     public R<String> delete(@RequestParam List<Long> ids) {
         log.info("ids:{}", ids);
@@ -82,6 +87,7 @@ public class SetmealController {
         return R.success("套餐数据删除成功");
     }
 
+    @Cacheable(value="setmealCache",key="#setmeal.categoryId + '_'+ #setmeal.status")
     @GetMapping("/list")
     public R<List<Setmeal>> list(Setmeal setmeal) {
         LambdaQueryWrapper<Setmeal> lqw = new LambdaQueryWrapper<>();
